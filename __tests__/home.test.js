1c29172088187dd1f13444fda62c0c0d20331390
Vinicius Arruda <viniarruda.souza@gmail.com>
2019-02-27 22:34:29 +0000

    Adding test with redux mock

--- __tests__/home.test.js
+++ __tests__/home.test.js
@@ -3,11 +3,28 @@ import { shallow } from 'enzyme'
 
 import Home from '../src/components/views/home'
 
+import configureStore from 'redux-mock-store'
+
+const mockStore = configureStore()
+
+const initialState = {
+  github: {
+    loading: false,
+    list: null,
+    error: null,
+    user: null,
+    commits: null
+  }
+}
+
+const store = mockStore(initialState)
+
 describe('Testing Home Component', () => {
  it('should render correctly', () => {
    const wrapper = shallow(
-     <Home />
-   )
+     <Home />,
+      { context: { store } }
+   ).dive();
 
    expect(wrapper).toMatchSnapshot()
  })
